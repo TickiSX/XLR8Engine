4560babc9d0c00c8834e0ea2b63b3d8d586e7476
TickiSX <cgonzalezgg0@gmail.com>
2025-05-19 08:45:56 -0600

    xd

--- XLR8Engine/xd/include/BaseApp.h
+++ XLR8Engine/xd/include/BaseApp.h
@@ -1,1 +1,32 @@
 #pragma once
+#include "Prerequisites.h"
+
+class BaseApp
+{
+public:
+	BaseApp() = default;
+	~BaseApp();
+
+	int run();
+
+	bool init();
+
+	void update();
+
+	void render();
+
+	void destroy();
+
+	void handleEvents();
+
+private:
+
+};
+
+BaseApp::BaseApp()
+{
+}
+
+BaseApp::~BaseApp()
+{
+}
---
+++ XLR8Engine/xd/src/BaseApp.cpp
@@ -0,0 +1,15 @@
+#include "../include/BaseApp.h"
+
+int 
+BaseApp::run() {
+	return 0;
+}
+
+bool
+BaseApp::init() {
+	return false;
+}
+
+int
+BaseApp::update() {
+}
